183eda30ee7549ab5840ede8d4148e0c526336ad
Kyle <schoenbergerk@hotmail.com>
2024-05-17 22:33:32 -0500

    setup starting inquirer page

--- index.js
+++ index.js
@@ -10,3 +10,26 @@ const connection = mysql.createConnection({
     database: "employeeTrackerdb",
 });
 
+connection.connect((err) => {
+    if (err) throw err;
+    console.log("Connected!");;
+    initialize();
+});
+
+function initialize() {
+    inquirer
+    .prompt({
+        type: "list",
+        name: "action",
+        message: "Please select an option",
+        choices: [
+            "View all Departments",
+            "View all roles",
+            "View all employees",
+            "Add a department",
+            "Add a role",
+            "Add an employee",
+            "Update an employee role",
+        ]
+    })
+}
