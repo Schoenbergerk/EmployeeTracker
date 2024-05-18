4beaefd36c996eaee6403add0adf745b6cbb069c
Kyle <schoenbergerk@hotmail.com>
2024-05-18 10:11:10 -0500

    setup database things

---
+++ db/schema.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS employeeTracker_db;
+
+CREATE DATABASE employeeTracker_db;
+
+USE employeeTracker_db;
+
+CREATE TABLE departments (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    department_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL(10, 2),
+    FOREIGN KEY (department_id),
+    REFERENCES departments (id),
+);
+
+CREATE TABLE employees (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    Foreign Key (role_id) REFERENCES role(id),
+    Foreign Key (manager_id) REFERENCES employee(id),
+)
